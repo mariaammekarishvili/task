219942e66344c8edf3a222214edc5d943dbfd1f2
PersonalMariamMek <mariam.mekarishvili@gmail.com>
2024-02-15 13:23:39 +0400

    fix: theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,20 @@
 import { useLayoutEffect } from "react";
 import { parseCookies } from "nookies";
 import { useRouter } from "next/navigation";
+import "tailwindcss/tailwind.css";
+import { useTheme } from "next-themes";
 
 import LogIn from "@/pages/logIn";
 
 export default function Home() {
   const cookies = parseCookies();
   const router = useRouter();
+  const { setTheme } = useTheme();
 
   useLayoutEffect(() => {
+    setTheme("light")
     if (!cookies.token) {
-      router.push("/logIn");
+      router.push("/");
     } else {
       router.push("/users");
     }
